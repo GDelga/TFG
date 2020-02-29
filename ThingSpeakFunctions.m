899601aeeb3dca4e346acda7db348d1eb3fd9b83
GDelga <gdelga02@ucm.es>
2020-02-29 19:54:39 +0100

    Forecast funcionality

--- ThingSpeakFunctions.m
+++ ThingSpeakFunctions.m
@@ -6,5 +6,11 @@ classdef ThingSpeakFunctions
       function [data,timestamps,channelInfo] = read(obj, channelID, APIKey, labels)
          [data,timestamps,channelInfo] = thingSpeakRead(channelID, 'Fields', labels, 'Readkey', APIKey);
       end
+      function [data,timestamps,channelInfo] = readWithNumber(obj, channelID, APIKey, labels, number)
+         [data,timestamps,channelInfo] = thingSpeakRead(channelID, 'Fields', labels, 'NumPoints', number, 'Readkey', APIKey);
+      end
+      function [data,timestamps,channelInfo] = readWithRange(obj, channelID, APIKey, labels, t1, t2)
+         [data,timestamps,channelInfo] = thingSpeakRead(channelID, 'Fields', labels, 'DateRange',[t1,t2], 'Readkey', APIKey);
+      end
    end
 end
