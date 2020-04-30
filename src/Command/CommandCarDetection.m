2636df30cf20b3d1e0dd00573a51ed850364d337
GDelga <gdelga02@ucm.es>
2020-04-30 21:40:40 +0200

    añadir fichero

--- src/Command/CommandCarDetection.m
+++ src/Command/CommandCarDetection.m
@@ -3,8 +3,10 @@ classdef CommandCarDetection
     methods
         
         function r = execute(obj, data)
+            import src.AS.*
+            import src.Context.*
             result = ASFactory.getInstance().createASSmartCities().detection(data);
-            if(isnumeric(r) ismethod(result,'isnan'))
+            if(~isnumeric(r) && ismethod(result,'isnan'))
                 r = Context(Events.EXECUTE_CAR_DETECTION_OK, result);
             elseif(isnumeric(result) && result == 1)
                 r = Context(Events.EXECUTE_CAR_DETECTION_STOP, 'The video has been stopped');
