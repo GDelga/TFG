84641e558d3338c86ecfc93a4f9cd7e644edda3d
GDelga <gdelga02@ucm.es>
2020-04-30 20:42:35 +0200

    Forecast terminado

--- CommandForecastWrite.m
+++ CommandForecastWrite.m
@@ -4,7 +4,7 @@ classdef CommandForecastWrite
         
         function r = execute(obj, data)
             result = ASFactory.getInstance().createASThingSpeak().write(data);
-            r = Context(Events.EXECUTE_FORECAST_WRITE, data.getData{2});
+            r = Context(Events.EXECUTE_FORECAST_WRITE, data.getData());
         end
         
     end
