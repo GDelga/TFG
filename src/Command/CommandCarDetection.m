34a54024615164d7a61862befc63a2cc107a1407
GDelga <gdelga02@ucm.es>
2020-05-05 18:14:09 +0200

    Update CommandCarDetection.m

--- src/Command/CommandCarDetection.m
+++ src/Command/CommandCarDetection.m
@@ -6,10 +6,10 @@ classdef CommandCarDetection
             import src.AS.*
             import src.Context.*
             result = ASFactory.getInstance().createASSmartCities().detection(data);
-            if(~isnumeric(result) && ismethod(result,'isnan'))
-                r = Context(Events.EXECUTE_CAR_DETECTION_OK, result);
-            elseif(isnumeric(result) && result == 1)
+            if(isnumeric(result) && result == 1)
                 r = Context(Events.EXECUTE_CAR_DETECTION_STOP, 'The video has been stopped');
+            elseif(ismethod(result,'isnan'))
+                r = Context(Events.EXECUTE_CAR_DETECTION_OK, result);
             else
                 r = Context(Events.EXECUTE_CAR_DETECTION_KO, result);
             end
